8eb1fb517e88e1e1fc4fe3b49e483a91bc4b0cc1
SanjanaChintawar <sanjanachintawar007@gmail.com>
2026-06-22 01:06:36 +0530

    Frequency Count of Numbers, getOrDefult()

---
+++ frequencyCountNums/freqNums.java
@@ -0,0 +1,31 @@
+
+import java.util.HashMap;
+
+public class freqNums {
+    public static void main(String[] args){
+        int[] arr = {1,1,2,3,2,4,1,3,2,2,4,3,3,3,5};
+
+        // lets create a HashMap
+        // num -> freq count
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        // lets count the freqency
+        for(int num : arr){
+            // if(freq.containsKey(num)){
+            //     freq.put(num, freq.get(num)+1);
+            // }
+            // else{
+            //     freq.put(num, 1);
+            // }
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        System.out.println(freq);
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        map.put(1, 5);
+
+        System.out.println(map.getOrDefault(2, 0));
+    }
+}
