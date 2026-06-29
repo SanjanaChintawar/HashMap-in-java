b80c206631f430f321173b7a192a5afb94ea878a
SanjanaChintawar <sanjanachintawar007@gmail.com>
2026-06-29 22:57:49 +0530

    Word freqency count

--- wordFrequency/word.java
+++ wordFrequency/word.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class word {
     public static void main(String[] args) {
         String sentence = "Java is fun Java is easy and powerful Learning Java with HashMap is fun because HashMap makes many problems easy. and Java Java Java helps improve programming skills.";
-        // Remove punctuation
+        // Remove punctuation 
         sentence = sentence.replaceAll("[^a-zA-Z ]", "");
         String[] words = sentence.split(" ");
 
